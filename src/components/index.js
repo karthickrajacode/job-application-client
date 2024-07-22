c432996b25180e3c32bbda079cd0c8d904b19804
karthick <kart87632@gmail.com>
2024-07-22 15:40:24 +0530

    Updated TextInput & SignUp  a index.js  in components.

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
-import Navbar from "./Navbar";
-import Footer from "./Footer";
-import CustomButton from "./CustomButton";
+import Navbar from "./Navbar.jsx";
+import Footer from "./Footer.jsx";
+import CustomButton from "./CustomButton.jsx";
+import TextInput from "./TextInput.jsx";
+import SignUp from "./SignUp.jsx";
 
-export {Navbar,Footer,CustomButton};
+export { Navbar, Footer, CustomButton, TextInput, SignUp };
