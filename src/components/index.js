d7eb19434f860d1dc2e17de37df11d9df510576d
karthick <kart87632@gmail.com>
2024-07-22 19:10:22 +0530

    Updated a import JobCard & ListBox  index.js in components.

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,7 @@ import CustomButton from "./CustomButton.jsx";
 import TextInput from "./TextInput.jsx";
 import SignUp from "./SignUp.jsx";
 import Header from "./Header.jsx";
+import JobCard from "./JobCard.jsx"
+import ListBox from "./ListBox.jsx";
 
-export { Navbar, Footer, CustomButton, TextInput, SignUp ,Header};
+export { Navbar, Footer, CustomButton, TextInput, SignUp ,Header, JobCard, ListBox};
