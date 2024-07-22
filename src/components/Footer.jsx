4b8e2fc38a5a043c29d3bb6aa9a99c6683d0b1fc
karthick <kart87632@gmail.com>
2024-07-22 16:38:43 +0530

    Updated a Footer.jsx in components.(Designed by karthi)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -103,7 +103,7 @@ const Footer = () => {
             </p>
 
             <span className="sm:ml-auto sm:mt-0 mt-2 sm:w-auto w-full sm:text-left text-center text-gray-300 text-sm">
-              Designed by CodeWave
+              Designed by karthi
             </span>
           </div>
         </div>
