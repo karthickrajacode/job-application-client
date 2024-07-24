9b6589231f119e20ddef5b8f8b23ce1bf84faa68
karthick <kart87632@gmail.com>
2024-07-24 15:16:47 +0530

    Updated a import a JobTyps index.js in components.

--- src/components/index.js
+++ src/components/index.js
@@ -4,20 +4,22 @@ import CustomButton from "./CustomButton.jsx";
 import TextInput from "./TextInput.jsx";
 import SignUp from "./SignUp.jsx";
 import Header from "./Header.jsx";
-import JobCard from "./JobCard.jsx"
+import JobCard from "./JobCard.jsx";
 import ListBox from "./ListBox.jsx";
 import Loading from "./Loading.jsx";
 import CompanyCard from "./CompanyCard.jsx";
+import JobTypes from "./JobTypes.jsx";
 
-export { 
-    Navbar, 
-    Footer, 
-    CustomButton, 
-    TextInput,
-     SignUp,
-     Header, 
-     JobCard, 
-     ListBox,
-     Loading,
-     CompanyCard
-    };
+export {
+  Navbar,
+  Footer,
+  CustomButton,
+  TextInput,
+  SignUp,
+  Header,
+  JobCard,
+  ListBox,
+  Loading,
+  CompanyCard,
+  JobTypes,
+};
