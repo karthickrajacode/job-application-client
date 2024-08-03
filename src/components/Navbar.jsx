2fc08049cc0c716350ff4ed389b7cb3dc3a87927
Karthick <kart87632@gmail.com>
2024-08-03 19:01:44 +0530

    Updated a Navbar.jsx. changed isActive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -83,16 +83,16 @@ function MenuList({ user, onClick }) {
               </MenuItem>
 
               <MenuItem>
-                {({ isActive  }) => (
+                {({ isActive }) => (
                   <button
                     onClick={() => handleLogout()}
                     className={`${
-                      isActive  ? "bg-blue-500 text-white" : "text-gray-900"
+                      isActive ? "bg-blue-500 text-white" : "text-gray-900"
                     } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
                   >
                     <AiOutlineLogout
                       className={`${
-                        isActive  ? "text-white" : "text-gray-600"
+                        isActive ? "text-white" : "text-gray-600"
                       } mr-2 h-5 w-5  `}
                       aria-hidden="true"
                     />
@@ -132,7 +132,15 @@ const Navbar = () => {
               <Link to="/companies">Companies</Link>
             </li>
             <li>
-              <Link to="/upload-job">Upload Job</Link>
+              <Link
+                to={
+                  user?.accountType === "seeker"
+                    ? "/applications"
+                    : "/upload-job"
+                }
+              >
+                {user?.accountType === "seeker" ? "Applications" : "Upload Job"}
+              </Link>
             </li>
             <li>
               <Link to="/about-us">About</Link>
