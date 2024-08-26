e868200890fab07aea8b2816a924df122dd0a598
karthick <kart87632@gmail.com>
2024-08-26 11:47:39 +0530

    Updated a Companies.jsx debugging

--- src/pages/Companies.jsx
+++ src/pages/Companies.jsx
@@ -7,14 +7,13 @@ import {
   ListBox,
   Loading,
 } from "../components";
-import { companies } from "../utils/data";
 import { apiRequest, updateURL } from "../utils";
 
 const Companies = () => {
   const [page, setPage] = useState(1);
   const [numPage, setNumPage] = useState(1);
   const [recordsCount, setRecordsCount] = useState(0);
-  const [data, setData] = useState(companies ?? []);
+  const [data, setData] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
   const [cmpLocation, setCmpLocation] = useState("");
   const [sort, setSort] = useState("Newest");
@@ -48,18 +47,20 @@ const Companies = () => {
       setIsFetching(false);
     } catch (error) {
       console.log(error);
+      setIsFetching(false);
     }
   };
-  const handleSearchSubmit = async(error) => {
-    error.preventDefault()
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
 
-    await fetchCompanies()
+    fetchCompanies();
   };
   const handleShowMore = () => {};
 
   useEffect(() => {
     fetchCompanies();
   }, [page, sort]);
+
   return (
     <div className="w-full">
       <Header
@@ -74,8 +75,8 @@ const Companies = () => {
       <div className="container mx-auto flex flex-col gap-5 2xl:gap-10 px-5 md:px-0 py-6 bg-[#f7fdfd]">
         <div className="flex items-center justify-between mb-4">
           <p className="text-sm md:text-base">
-            Shwoing: <span className="font-semibold">{recordsCount}</span> Companies
-            Available
+            Shwoing: <span className="font-semibold">{recordsCount}</span>{" "}
+            Companies Available
           </p>
 
           <div className="flex flex-col md:flex-row gap-0 md:gap-2 md:items-center">
